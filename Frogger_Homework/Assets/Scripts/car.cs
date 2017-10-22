092f6de49a3ee0c739f915f28d6bd94405b18111
jalpendesai <djalpen24@gmail.com>
2017-10-22 14:55:30 -0700

    Added Frog & car movement

---
+++ Frogger_Homework/Assets/Scripts/car.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+
+public class car : MonoBehaviour {
+
+    public Rigidbody2D rb;
+
+    float speed = 1f;
+
+    // [Optional] for setting the random speed range
+    // public float minSpeed = 3f;
+    // public float maxSpeed = 7f;
+
+    void Start()
+    {
+        // speed = Random.Range(minSpeed, maxSpeed);       
+    }
+
+    // FixedUpdate is meant for physics, Update is meant for everything else.
+    void FixedUpdate () {
+        Vector2 forward = new Vector2(transform.right.x, transform.right.y);
+        rb.MovePosition(rb.position + forward * Time.fixedDeltaTime * speed);
+		
+	}
+}
