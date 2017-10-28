63d1107a1a3a5acd9d09293fb662e272895ec669
jalpendesai <djalpen24@gmail.com>
2017-10-28 00:22:21 -0700

    Added specific time interval CarSpawners

---
+++ Frogger_Homework/Assets/Scripts/CarSpawn.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSpawn : MonoBehaviour {
+
+    public float spawnDelay = .9f;
+    public GameObject car;
+    public Transform[] SpawnPoints;
+
+    float nextTimeToSpawn = 0f;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnCars", 0f, 1f); 
+    }
+
+    // Update is called once per frame
+    /*void Update () {
+		if(nextTimeToSpawn <= Time.time)
+        {
+            SpawnCars();
+            nextTimeToSpawn = Time.time + spawnDelay;
+        }
+	}*/
+
+    void SpawnCars()
+    {
+        int randomIndex = Random.Range(0, SpawnPoints.Length);
+        Transform SpawnPoint = SpawnPoints[randomIndex];
+
+        Instantiate(car, SpawnPoint.position, SpawnPoint.rotation);
+    }
+}
